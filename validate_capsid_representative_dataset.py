8d760709ed3d85c6c62a01c016f1c3ac71f5ee0f
muhit-emon <82660145+muhit-emon@users.noreply.github.com>
2021-06-07 22:29:35 -0400

    Update validate_capsid_representative_dataset.py

--- validate_capsid_representative_dataset.py
+++ validate_capsid_representative_dataset.py
@@ -1,6 +1,9 @@
 from Bio import SeqIO
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 import json
 
 import tensorflow as tf
@@ -15,7 +18,8 @@ from keras.models import Sequential, load_model, save_model, Model
 from keras.optimizers import Adam, Adagrad
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, roc_auc_score
 
 
 def make_binary_for_3_mers(index):
@@ -303,8 +307,154 @@ def create_3_mer_one_hot_vectors(positive_proteins, negative_proteins):
     X_3_mer_one_hot_vectors = np.vstack(X_3_mer_one_hot_vectors)
     return X_3_mer_one_hot_vectors
 
+
+def create_inputs_for_hydropathy(positive_proteins, negative_proteins):
+    window_size = 15 # window to find the average hydropathy of protein regions 
+    L = 350 # we are using 350 as length of a row in the 3d input matrix as 95% capsid and non-capsid proteins have length less than 350
+    X_hydropathy = []
+    for i in range(len(negative_proteins)):
+        neg_protein = negative_proteins[i].seq
+        
+        if len(neg_protein) <= L:
+            tmp_hydropathy = np.zeros((1, L, 1)) # 1-mer one hot vetors, for neg_protein
+            window_str = neg_protein[0:window_size]
+            sum_of_kd_values = 0
+            for aa in window_str:
+                sum_of_kd_values+=hydropathicity_of_aa[aa]
+            x = np.zeros((1, 1))
+            x[0][0] = sum_of_kd_values / window_size
+            for j in range(1, len(neg_protein)-window_size+1):
+                x = np.zeros((1, 1))
+                window_str = neg_protein[j:j+window_size]
+                sum_of_kd_values-=hydropathicity_of_aa[neg_protein[j-1]]
+                sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                x[0][0] = sum_of_kd_values / window_size
+                tmp_hydropathy[0][j] = x
+            X_hydropathy.append(tmp_hydropathy)
+            #Y_train.append(0)
+        else:
+            number_of_chunks = len(neg_protein) // L
+            remainder_chunk = len(neg_protein) % L
+            start = 0
+            end = L
+            for k in range(number_of_chunks):
+                chunk = neg_protein[start:end]
+                tmp_hydropathy = np.zeros((1, L, 1)) # 1-mer one hot vetors, for chunk
+                pos = 0
+                window_str = chunk[0:window_size]
+                sum_of_kd_values = 0
+                for aa in window_str:
+                    sum_of_kd_values+=hydropathicity_of_aa[aa]
+                x = np.zeros((1, 1))
+                x[0][0] = sum_of_kd_values / window_size
+                for j in range(1, len(chunk)-window_size+1):
+                    x = np.zeros((1, 1))
+                    window_str = chunk[j:j+window_size]
+                    sum_of_kd_values-=hydropathicity_of_aa[chunk[j-1]]
+                    sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                    x[0][0] = sum_of_kd_values / window_size
+                    tmp_hydropathy[0][pos] = x
+                    pos += 1
+                X_hydropathy.append(tmp_hydropathy)
+                #Y_train.append(0)
+                start = end
+                end = start + L
+            if remainder_chunk:
+                chunk = neg_protein[start:start+remainder_chunk]
+                tmp_hydropathy = np.zeros((1, L, 1)) # 1-mer one hot vetors, for chunk
+                pos = 0
+                window_str = chunk[0:window_size]
+                sum_of_kd_values = 0
+                for aa in window_str:
+                    sum_of_kd_values+=hydropathicity_of_aa[aa]
+                x = np.zeros((1, 1))
+                x[0][0] = sum_of_kd_values / window_size
+                for j in range(1, len(chunk)-window_size+1):
+                    x = np.zeros((1, 1))
+                    window_str = chunk[j:j+window_size]
+                    sum_of_kd_values-=hydropathicity_of_aa[chunk[j-1]]
+                    sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                    x[0][0] = sum_of_kd_values / window_size
+                    tmp_hydropathy[0][pos] = x
+                    pos += 1
+                X_hydropathy.append(tmp_hydropathy)
+                #Y_train.append(0)
+
+    for i in range(len(positive_proteins)):
+        post_protein = positive_proteins[i].seq
+        
+        if len(post_protein) <= L:
+            tmp_hydropathy= np.zeros((1, L, 1)) # 1-mer one hot vetors, for neg_protein
+            window_str = post_protein[0:window_size]
+            sum_of_kd_values = 0
+            for aa in window_str:
+                sum_of_kd_values+=hydropathicity_of_aa[aa]
+            x = np.zeros((1, 1))
+            x[0][0] = sum_of_kd_values / window_size
+            for j in range(1, len(post_protein)-window_size+1):
+                x = np.zeros((1, 1))
+                window_str = post_protein[j:j+window_size]
+                sum_of_kd_values-=hydropathicity_of_aa[post_protein[j-1]]
+                sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                x[0][0] = sum_of_kd_values / window_size
+                tmp_hydropathy[0][j] = x
+            X_hydropathy.append(tmp_hydropathy)
+            #Y_train.append(1)
+        else:
+            number_of_chunks = len(post_protein) // L
+            remainder_chunk = len(post_protein) % L
+            start = 0
+            end = L
+            for k in range(number_of_chunks):
+                chunk = post_protein[start:end]
+                tmp_hydropathy = np.zeros((1, L, 1)) # 1-mer one hot vetors, for chunk
+                pos = 0
+                window_str = chunk[0:window_size]
+                sum_of_kd_values = 0
+                for aa in window_str:
+                    sum_of_kd_values+=hydropathicity_of_aa[aa]
+                x = np.zeros((1, 1))
+                x[0][0] = sum_of_kd_values / window_size
+                for j in range(1, len(chunk)-window_size+1):
+                    x = np.zeros((1, 1))
+                    window_str = chunk[j:j+window_size]
+                    sum_of_kd_values-=hydropathicity_of_aa[chunk[j-1]]
+                    sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                    x[0][0] = sum_of_kd_values / window_size
+                    tmp_hydropathy[0][pos] = x
+                    pos += 1
+                X_hydropathy.append(tmp_hydropathy)
+                #Y_train.append(1)
+                start = end
+                end = start + L
+            if remainder_chunk:
+                chunk = post_protein[start:start+remainder_chunk]
+                tmp_hydropathy = np.zeros((1, L, 1)) # 1-mer one hot vetors, for chunk
+                pos = 0
+                window_str = chunk[0:window_size]
+                sum_of_kd_values = 0
+                for aa in window_str:
+                    sum_of_kd_values+=hydropathicity_of_aa[aa]
+                x = np.zeros((1, 1))
+                x[0][0] = sum_of_kd_values / window_size
+                for j in range(1, len(chunk)-window_size+1):
+                    x = np.zeros((1, 1))
+                    window_str = chunk[j:j+window_size]
+                    sum_of_kd_values-=hydropathicity_of_aa[chunk[j-1]]
+                    sum_of_kd_values+=hydropathicity_of_aa[window_str[-1]]
+                    x[0][0] = sum_of_kd_values / window_size
+                    tmp_hydropathy[0][pos] = x
+                    pos += 1
+                X_hydropathy.append(tmp_hydropathy)
+                #Y_train.append(1)
+
+    X_hydropathy = np.vstack(X_hydropathy)
+    return X_hydropathy
+
+
 non_structural_proteins = list(SeqIO.parse("../capsid_testing/representative/negative.fasta", "fasta"))
 capsid_proteins = list(SeqIO.parse("../capsid_testing/representative/positive.fasta", "fasta"))
+
 
 Y_test_true = []
 
@@ -342,7 +492,7 @@ for i in range(len(capsid_proteins)):
     Y_test_true.append(1)
 
 Y_test_true = np.array(Y_test_true)
-
+hydropathicity_of_aa = {'A':1.8, 'C':2.5, 'D':-3.5, 'E':-3.5, 'F':2.8, 'G':-0.4, 'H':-3.2, 'I':4.5, 'K':-3.9, 'L':3.8, 'M':1.9, 'N':-3.5, 'P':-1.6, 'Q':-3.5, 'R':-4.5, 'S':-0.8, 'T':-0.7, 'V':4.2, 'W':-0.9, 'Y':-1.3}
 with open('index_of_kmers.json') as index_file:
     kmers_index = json.load(index_file)
 
@@ -351,24 +501,103 @@ X_1_mers = create_1_mer_one_hot_vectors(capsid_proteins, non_structural_proteins
 X_2_mers = create_2_mer_one_hot_vectors(capsid_proteins, non_structural_proteins)
 #print(X_2_mers.shape)
 X_3_mers = create_3_mer_one_hot_vectors(capsid_proteins, non_structural_proteins)
+X_4_hydropathy = create_inputs_for_hydropathy(capsid_proteins, non_structural_proteins)
 
 # evaluate saved model on validation dataset with 960 capsid(p) proteins and 1576 non-structural(n) proteins
-model = load_model('capsid_3channels.h5')
-Y_prediction = model.predict([X_1_mers, X_2_mers, X_3_mers])
+model_2chn = load_model('capsid_2channels.h5')
+model_3chn = load_model('capsid_3channels.h5')
+model_4chn = load_model('capsid_4channels_with_hydropathy.h5')
+Y_prediction_2chn = model_2chn.predict([X_1_mers, X_2_mers])
+Y_prediction_3chn = model_3chn.predict([X_1_mers, X_2_mers, X_3_mers])
+Y_prediction_4chn = model_4chn.predict([X_1_mers, X_2_mers, X_3_mers, X_4_hydropathy])
 
-Y_prediction_processed = []
+result_table = pd.DataFrame(columns=['classifiers', 'fpr','tpr','auc'])
+
+
+Y_prediction_processed_2chn = []
 for i in range(len(spans_of_chunks)):
     s, e = spans_of_chunks[i]
     mx = -1000
     for j in range(s, e+1):
-        mx = max(mx, Y_prediction[j][0])
-    Y_prediction_processed.append(mx)
+        mx = max(mx, Y_prediction_2chn[j][0])
+    Y_prediction_processed_2chn.append(mx)      # Y_prediction_processed contains the predicted score for each protein sequence
+
+Y_prediction_processed_3chn = []
+for i in range(len(spans_of_chunks)):
+    s, e = spans_of_chunks[i]
+    mx = -1000
+    for j in range(s, e+1):
+        mx = max(mx, Y_prediction_3chn[j][0])
+    Y_prediction_processed_3chn.append(mx)
+
+
+Y_prediction_processed_4chn = []
+for i in range(len(spans_of_chunks)):
+    s, e = spans_of_chunks[i]
+    mx = -1000
+    for j in range(s, e+1):
+        mx = max(mx, Y_prediction_4chn[j][0])
+    Y_prediction_processed_4chn.append(mx)
+
+
+fpr, tpr, _ = roc_curve(Y_test_true,  Y_prediction_processed_2chn)
+auc = roc_auc_score(Y_test_true, Y_prediction_processed_2chn)
+    
+result_table = result_table.append({'classifiers':'model with 2 CNN channels',
+                                    'fpr':fpr, 
+                                    'tpr':tpr, 
+                                    'auc':auc}, ignore_index=True)
+
+
+fpr, tpr, _ = roc_curve(Y_test_true,  Y_prediction_processed_3chn)
+auc = roc_auc_score(Y_test_true, Y_prediction_processed_3chn)
+    
+result_table = result_table.append({'classifiers':'model with 3 CNN channels',
+                                    'fpr':fpr, 
+                                    'tpr':tpr, 
+                                    'auc':auc}, ignore_index=True)
+
+
+fpr, tpr, _ = roc_curve(Y_test_true,  Y_prediction_processed_4chn)
+auc = roc_auc_score(Y_test_true, Y_prediction_processed_4chn)
+    
+result_table = result_table.append({'classifiers':'model with 4 CNN channels',
+                                    'fpr':fpr, 
+                                    'tpr':tpr, 
+                                    'auc':auc}, ignore_index=True)
+
+# Set name of the classifiers as index labels
+result_table.set_index('classifiers', inplace=True)
+
+#plot the figure
+fig = plt.figure(figsize=(8,6))
+
+for i in result_table.index:
+    plt.plot(result_table.loc[i]['fpr'], 
+             result_table.loc[i]['tpr'], 
+             label="{}, AUC={:.3f}".format(i, result_table.loc[i]['auc']))
+    
+plt.plot([0,1], [0,1], color='orange', linestyle='--')
+
+plt.xticks(np.arange(0.0, 1.1, step=0.1))
+plt.xlabel("Flase Positive Rate", fontsize=15)
+
+plt.yticks(np.arange(0.0, 1.1, step=0.1))
+plt.ylabel("True Positive Rate", fontsize=15)
+
+plt.title('ROC Curve Analysis', fontweight='bold', fontsize=15)
+plt.legend(prop={'size':13}, loc='lower right')
+
+#plt.show()
+
+fig.savefig('multiple_roc_curve.png')
+
 
 th = 0.5
 
 Y_pred = [] # this list contains either 1 or 0 for depending on score threshold
-for i in range(len(Y_prediction_processed)):
-    if Y_prediction_processed[i] >= th:
+for i in range(len(Y_prediction_processed_3chn)):
+    if Y_prediction_processed_3chn[i] >= th:
         Y_pred.append(1)
     else:
         Y_pred.append(0)
